51d454dcf0571224642b1f99e05fa2eadca50151
Clifford <clifford@twjoin.com>
2022-01-14 11:10:13 +0800

    feat(header tab link): 首頁header連結

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/logo.svg';
 
 function ElevationScroll(props) {
   const { children, window } = props;
@@ -19,12 +27,99 @@ function ElevationScroll(props) {
   });
 }
 
+// https://v4.mui.com/zh/customization/default-theme/
+const useStyles = makeStyles(theme => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: '3em',
+  },
+  logo: {
+    height: '7em',
+  },
+  tabContainer: {
+    marginLeft: 'auto',
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: '25px',
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: '50px',
+    marginLeft: '50px',
+    marginRight: '25px',
+    height: '45px',
+  },
+}));
+
 export default function Header(props) {
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (e, value) => {
+    setValue(value);
+  };
   return (
-    <ElevationScroll>
-      <AppBar>
-        <Toolbar>Arc development</Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <>
+      {/* https://v4.mui.com/components/app-bar/ */}
+      <ElevationScroll>
+        <AppBar>
+          {/* 移除 toolbar 左右padding */}
+          <Toolbar disableGutters>
+            <img src={logo} className={classes.logo} alt="company logo" />
+            {/* https://v4.mui.com/zh/components/typography/ */}
+            {/* <Typography variant="h3">Arc development</Typography> */}
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              className={classes.tabContainer}
+              indicatorColor="primary"
+            >
+              {/* https://v4.mui.com/zh/components/tabs/ */}
+              <Tab
+                className={classes.tab}
+                component={Link}
+                label="Home"
+                to="/"
+              ></Tab>
+              <Tab
+                className={classes.tab}
+                component={Link}
+                label="Services"
+                to="/services"
+              ></Tab>
+              <Tab
+                className={classes.tab}
+                component={Link}
+                label="The Revolution"
+                to="/revolution"
+              ></Tab>
+              <Tab
+                className={classes.tab}
+                component={Link}
+                label="About Us"
+                to="/about"
+              ></Tab>
+              <Tab
+                className={classes.tab}
+                component={Link}
+                label="Contact Us"
+                to="/contact"
+              ></Tab>
+            </Tabs>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+            >
+              Free Estimate
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      {/* header與內容之間的緩衝 吃到default tool bar的高度*/}
+      <div className={classes.toolbarMargin}></div>
+    </>
   );
 }
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,10 +1,40 @@
 import { createTheme } from '@material-ui/core/styles';
+// https://v4.mui.com/zh/customization/theming/
 
+const arcBlue = '#0B72b9';
+const arcOrange = '#FFbA60';
+
+// https://v4.mui.com/zh/customization/default-theme/
 export default createTheme({
   palette: {
     common: {
-      arcBlue: '#0B72b9',
-      arcOrange: '#FFbA60',
+      blue: `${arcBlue}`,
+      orange: `${arcOrange}`,
+    },
+    primary: {
+      main: `${arcBlue}`,
+    },
+    secondary: {
+      main: `${arcOrange}`,
+    },
+  },
+  //   https://v4.mui.com/zh/components/typography/
+  typography: {
+    h3: {
+      fontWeight: 300,
+    },
+    tab: {
+      fontFamily: 'Raleway',
+      // 不要讓他全部轉大寫
+      textTransform: 'none',
+      fontWeight: 700,
+      fontSize: '1rem',
+    },
+    estimate: {
+      fontFamily: 'Pacifico',
+      fontSize: '1rem',
+      textTransform: 'none',
+      color: 'white'
     },
   },
 });
